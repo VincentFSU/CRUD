4fcc266c0dcfd04ddb3b22f25ec25b99385f7504
VincentFSU <54596770+VincentFSU@users.noreply.github.com>
2020-03-19 19:55:26 -0400

    Added Project Working Files

---
+++ CRUD/Program.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CRUD
+{
+    class Program
+    {
+        public static void Main()
+        {
+            bool running = true;
+
+            /* LIST ASSERTS THAT EACH USER HAS A UNIQUE ID
+             * ALLOWS FOR USERS OF SAME PREFIX AND SUFFIX */
+
+            //establishes an empty list of users
+            List<User> userList = new List<User>();
+
+
+            //menu
+            Console.WriteLine("Welcome to you user database! \n");
+            Console.WriteLine("Please choose one of these menu options: ");
+
+            string choice = "null";
+
+            //ask for input over and over until the user quits
+            do
+            {
+
+                Console.WriteLine("[ C - Create | R - Read | U - Update | D - Delete | P - Print | Q - Quit ]");
+                Console.Write("Input: ");
+
+                //user menu choice
+                choice = Console.ReadLine();
+                Console.WriteLine();
+
+                //add a new user to the system
+                if (choice == "C" || choice == "c")
+                {
+                    //prompt for entering a new prefix, suffix, and userId
+                    Console.Write("Please enter a prefix: ");
+                    string prefix = Console.ReadLine();
+                    Console.Write("Please enter a suffix: ");
+                    string suffix = Console.ReadLine();
+
+                    //is a new entry allowed
+                    bool entryAllowed = true;
+
+                    do
+                    {
+                        Console.Write("Please enter an ID: ");
+                        string userID = Console.ReadLine();
+
+                        //is the user allowed to be entered into the database
+                        entryAllowed = true;
+
+                        //checks to see if the user id is already in use (i.e. if there are duplicates)
+                        foreach (User user in userList)
+                        {
+                            if (user.getUserID() == userID)
+                            {
+                                Console.WriteLine("\nA user with this ID already exists, please try again.\n");
+                                entryAllowed = false;
+                            }
+                        }
+
+                        //if there are no duplicates, the user is added into the database
+                        if (entryAllowed)
+                        {
+                            userList.Add(new User(prefix, suffix, userID));
+                            Console.WriteLine("\n" + userList.Last().tostring() + "\n");
+                        }
+                    } while (!entryAllowed);
+                    { }
+
+
+                }
+
+                //read information of an existing user
+                if (choice == "R" || choice == "r")
+                {
+                    //prompt to enter in the user's id to match to an existing one
+                    Console.Write("Please enter the user's ID: ");
+                    string userID = Console.ReadLine();
+
+                    //counter which will increment with each matching instance
+                    int count = 0;
+
+                    //if a user with the same id as the entered one exists, return it's information
+                    foreach (User user in userList)
+                    {
+                        if (user.getUserID() == userID)
+                        {
+                            count++;
+                            Console.WriteLine("\n" + userID + ": " + user.tostring() + "\n");
+                        }
+                    }
+
+                    //if no user is found (i.e. count is 0), return error
+                    if (count == 0)
+                    {
+                        Console.WriteLine("\n ***No such user*** \n");
+                    }
+                }
+
+                //update a users information
+                if (choice == "U" || choice == "u")
+                {
+
+                    //enter the users id
+                    Console.Write("Please enter the user's ID: ");
+                    string userID = Console.ReadLine();
+
+                    //declare new values
+                    string newPref;
+                    string newSuf;
+                    string newID;
+
+                    //is the newID available?
+                    bool idAvailable = true;
+
+                    //when the user's id is matched, take input to determine new
+                    //values for prefix, suffix, and id
+                    foreach (User user in userList)
+                    {
+                        if (user.getUserID() == userID)
+                        {
+                            Console.WriteLine("\nCurrent information: " + user.tostring());
+                            Console.Write("\nPlease enter the user's new prefix: ");
+                            newPref = Console.ReadLine();
+                            Console.Write("Please enter the user's new suffix: ");
+                            newSuf = Console.ReadLine();
+                            do
+                            {
+                                //id is initially available with each run of loop
+                                idAvailable = true;
+
+                                //enter new id
+                                Console.Write("Please enter the user's new ID: ");
+                                newID = Console.ReadLine();
+
+                                //checks to see if the user id is already in use (i.e. if there are duplicates)
+                                foreach (User us in userList)
+                                {
+
+                                    if (us.getUserID() == newID && newID != userID)
+                                    {
+                                        Console.WriteLine("\nA user with this ID already exists, please try again.\n");
+                                        idAvailable = false;
+                                    }
+                                }
+                            } while (!idAvailable);
+                            {
+                                user.setPrefix(newPref);
+                                user.setSuffix(newSuf);
+                                user.setUserID(newID);
+                                Console.WriteLine("\nUpdated information: " + user.tostring() + "\n");
+                            }
+                        }
+                    }
+                }
+
+                //delete a user of matching id
+                if (choice == "D" || choice == "d")
+                {
+                    //enter the user id
+                    Console.Write("Please enter the user's ID: ");
+                    string userID = Console.ReadLine();
+                    Console.WriteLine();
+
+                    //counter that increments with each matching id
+                    int count = 0;
+
+                    //when a matching id is found, remove the element at the ith index
+                    for (int i = 0; i < userList.Count; i++)
+                    {
+                        if (userList[i].getUserID() == userID)
+                        {
+                            count++;
+                            userList.RemoveAt(i);
+                        }
+                    }
+
+                    //if no matching id is found, return error
+                    if (count == 0)
+                    {
+                        Console.WriteLine("***No such user*** \n");
+                    }
+                }
+
+                //print all the information in the database
+                if (choice == "P" || choice == "p")
+                {
+                    for (int i = 0; i < userList.Count; i++)
+                    {
+                        Console.WriteLine(userList[i].tostring());
+                    }
+
+                    if (userList.Count == 0)
+                    {
+                        Console.WriteLine("To fill this empty table, enter 'C'.");
+                    }
+
+                    Console.WriteLine();
+                }
+
+                //quit the program
+                if (choice == "Q" || choice == "q")
+                {
+                    running = false;
+                }
+
+            } while (running);
+            { }
+        }
+    }
+}
---
+++ CRUD/User.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRUD
+{
+    public class User
+    {
+        string suffix;
+        string prefix;
+        string userID;
+
+        public User(string prefix, string suffix, string userID)
+        {
+            this.prefix = prefix;
+            this.suffix = suffix;
+            this.userID = userID;
+        }
+
+        public string getPrefix()
+        {
+            return this.prefix;
+        }
+
+        public string getSuffix()
+        {
+            return this.suffix;
+        }
+
+        public string getUserID()
+        {
+            return this.userID;
+        }
+
+        public void setPrefix(string prefix)
+        {
+            this.prefix = prefix;
+        }
+
+        public void setSuffix(string suffix)
+        {
+            this.suffix = suffix;
+        }
+
+        public void setUserID(string userID)
+        {
+            this.userID = userID;
+        }
+
+        public string tostring()
+        {
+            return "[Prefix == " + prefix + ", Suffix = " + suffix + ", userID = " + userID
+                + "]";
+        }
+    }
+}
